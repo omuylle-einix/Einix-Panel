35a12f4173ff74e44bfde7953646f2a9a0d0f614
Olivier Muylle <oliviermuylle@gmail.com>
2012-12-09 17:39:52 +0100

    Block Member fontionnel

--- nuked-klan version/block/membre.php
+++ nuked-klan version/block/membre.php
@@ -1,35 +1,39 @@
-		<div class="titre">Membres<div class="more"><a href="index.php?file=Members">more +</a></div></div>
-		<div id="memberc">
+<?php
+
+$sql = mysql_query("SELECT pseudo, date, avatar FROM " . $nuked['prefix'] . "_users ORDER BY date DESC LIMIT 0,8");
+
+$compteur = 0;
+
+while (list($pseudo) = mysql_fetch_array($sql))
+{
+	$titre = stripslashes($titre);
+	$avatar = !$user_data['avatar'] ? 'modules/User/images/noavatar.png' : checkimg($user_data['avatar']);
+
+	if (strlen($titre) > 25) $titre = substr($titre, 0, 25) . "(...)";
+	
+
+?>
 			<div class="memberpic">
-				<a href="#"><img src="themes/einix-panel/images/avatar.png"/></a>
-				<div class="mname">Cid</div>
-			</div>
-			<div class="memberpic">
-				<a href="#"><img src="http://love-design.eu/upload/User/1343943706.png"/></a>
-				<div class="mname">LoveForEver</div>
-			</div>	
-			<div class="memberpic">
-				<a href="#"><img src="http://tmnf.fr/criss/avatar2.jpg"/></a>
-				<div class="mname">Criss</div>
-			</div>	
-			<div class="memberpic">
-				<a href="#"><img src="http://img10.imageshack.us/img10/5224/c26d3261ca2e296a115acdc.png"/></a>
-				<div class="mname">Flux</div>
-			</div>		
-			<div class="memberpic">
-				<a href="#"><img src="http://www.nuked-klan.org/upload/User/1346851294.png"/></a>
-				<div class="mname">Nk`Pomme</div>
-			</div>
-			<div class="memberpic">
-				<a href="#"><img src="http://www.nuked-klan.org/upload/Forum/28076c04c5.jpg"/></a>
-				<div class="mname">Guezmer</div>
-			</div>
-			<div class="memberpic">
-				<a href="#"><img src="http://www.nuked-klan.org/modules/Forum/images/no_avatar.png"/></a>
-				<div class="mname">Hayanno</div>
-			</div>	
-			<div class="memberpic">
-				<a href="#"><img src="http://www.bia.teambattlefield.fr/upload/User/1337961349.png"/></a>
-				<div class="mname">abbe</div>
-			</div>				
-		</div>
+				<a href="#"><img src="<?php echo $avatar; ?>"/></a>
+				<div class="mname"><a href="index.php?file=Members&op=detail&autor=<?php echo $pseudo; ?>"><?php echo $pseudo; ?></div>
+			</div>			
+<?php
+	$compteur++;
+}
+
+if ($compteur == 8);
+else
+{
+	$na = 1;
+	$nbre_na = 8 - $compteur;
+	while ($na <= $nbre_na)
+	{
+		$na++;
+?>
+
+ <p>Aucun message n'a été enregistré..</p>
+
+<?php
+	}
+}
+?>
