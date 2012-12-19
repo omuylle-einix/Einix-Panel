76fe87c48fb524b5e7918533b6ae64c5bdf08121
Olivier Muylle <oliviermuylle@gmail.com>
2012-12-19 12:32:11 +0100

    Trying to show ctegory

--- nuked-klan version/block/telechargement.php
+++ nuked-klan version/block/telechargement.php
@@ -12,11 +12,27 @@ while (list($titre, $autor, $id) = mysql_fetch_array($sql))
 
 	if (strlen($titre) > 70) $titre = substr($titre, 0, 70) . "(...)";
 	
+        if ($visiteur >= $level)
+            $sql2 = mysql_query("SELECT titre, parentid FROM " . DOWNLOAD_CAT_TABLE . " WHERE cid = '" . $cat . "'");
+            list($cat_name, $parentid) = mysql_fetch_array($sql2);
+            $cat_name = printSecuTags($cat_name);
+
+            if ($cat == 0) {
+                $category = "N/A";
+            } else if ($parentid > 0) {
+                $sql3 = mysql_query("SELECT titre, parentid FROM " . DOWNLOAD_CAT_TABLE . " WHERE cid = '" . $parentid . "'");
+                list($parent_name) = mysql_fetch_array($sql3);
+                $parent_name = printSecuTags($parent_name);
+
+                $category = "<a href=\"index.php?file=Download&amp;op=categorie&amp;cat=" . $parentid . "\">" . $parent_name . "</a> -&gt; <a href=\"index.php?file=Download&amp;op=categorie&amp;cat=" . $cat . "\">" . $cat_name . "</a>";
+            } else {
+                $category = "<a href=\"index.php?file=Download&amp;op=categorie&amp;cat=" . $cat . "\">" . $cat_name . "</a>";
+            }
 
 ?>
 			<div class="nleft">
 				<div class="ntitle"><a href="index.php?file=Download&op=description&dl_id=<?php echo $id; ?>"><?php echo $titre; ?></a></div>
-				<div class="autor">Category: Xperia Miro</div>
+				<div class="autor">Category: <?php echo $category; ?></div>
 			</div>
 			<div class="download"><div class="sepl"></div><a href="index.php?file=Download&op=description&dl_id=<?php echo $id; ?>"><img src="themes/einix-panel/images/download.png" /></a></div>
 			<div class="clear"></div>
